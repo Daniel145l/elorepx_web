9c1b1bce868c444ec6913fe6205e78fc47e12a3e
Daniel Lucas <daniellucasm18@gmail.com>
2025-07-12 04:54:02 -0300

    Arquivo de carregamento de componentes

---
+++ js/include.js
@@ -0,0 +1,13 @@
+function includeComponent(id, file) {
+  fetch(file)
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById(id).innerHTML = data;
+  })
+  .cath((err) => console.log("Erro ao carregar no arquivo " + file, err));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  includeComponent('header', './components/header.html');
+  includeComponent('footer', './components/footer.html');
+})
