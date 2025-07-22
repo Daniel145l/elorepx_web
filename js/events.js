efead65ec2dbcdf814f9be9057479b1e036610d4
Daniel Lucas <daniellucasm18@gmail.com>
2025-07-22 18:40:47 -0300

    feat: consumindo dados da lua da api

--- js/events.js
+++ js/events.js
@@ -2,7 +2,6 @@
 const ipGeoKey = "45a5fe6795a244c89c4ddf7d2ff437f2";
 const apodKey = "Q7vAnoGct5NWdj2F7Yl0JMKw9ogZCrt3Wlvf7Y40";
 
-// const urlGeo = 'https://api.ipgeolocation.io/v2/astronomy?apiKey='+ipGeoKey; 
 
 //geolocationAPI
 async function carregaIpGeolocation(cidade = "") {
@@ -23,8 +22,34 @@ async function carregaIpGeolocation(cidade = "") {
     <div>
       <strong>Pôr do Sol em ${data.location.city}:</strong> <span class="text-gray-400">Fique de olho no céu às <strong>${data.astronomy.sunset}</strong> para ver o Sol se pôr em ${data.location.city}<span>
     </div>`;
+
+    document.getElementById("lua-info").innerHTML =
+    `<div>
+      <strong>Nascer da Lua em ${data.location.city}:</strong> <span class="text-gray-400">Olhe para o céu de ${data.location.city} às <strong>${data.astronomy.moonrise}</strong> para ver a Lua desaparecer<span>
+    </div>
+    <div>
+      <strong>Pôr da Lua em ${data.location.city}:</strong> <span class="text-gray-400">Olhe para o céu de ${data.location.city} às <strong>${data.astronomy.moonset}</strong> para ver a Lua aparecer<span>
+    </div>
+    <div>
+      <strong>Fase da Lua: </strong><span class="text-gray-400">A fase da Lua é <strong>${faseDaLuaEmPt(data.astronomy.moon_phase)}</strong></span>
+    </div>
+    `
   }catch(err) {
     console.log(err);
+  }
+}
+
+function faseDaLuaEmPt(fase) {
+  switch(fase) {
+    case "NEW_MOON": return "Lua Nova";
+    case "WAXING_CRESCENT": return "Lua Crescente";
+    case "FIRST_QUARTER": return "Quarto Crescente";
+    case "WAXING_GIBBOUS": return "Gibosa Crescente";
+    case "FULL_MOON": return "Lua Cheia";
+    case "WANING_GIBBOUS": return "Gibosa Minguante";
+    case "LAST_QUARTER": return "Quarto Minguante";
+    case "WANING_CRESCENT": return "Lua Minguante";
+    default: return "Fase desconhecida";
   }
 }
 
